31312c35481310cd96d014b17808f0a02784a9d9
kurbtica <146815673+kurbtica@users.noreply.github.com>
2025-12-17 10:37:05 +0100

    Intégration de l'environnement de tests et de la couverture 4

--- todo2025/vite.config.js
+++ todo2025/vite.config.js
@@ -5,10 +5,13 @@ import path from 'path'
 
 export default defineConfig({
     plugins: [
-        laravel({
-            input: ['resources/sass/app.scss', 'resources/js/app.js'],
-            refresh: true,
-        }),
+       laravel({
+        input: [
+            'resources/sass/app.scss',
+            'resources/js/app.js',
+        ],
+        refresh: true,
+    }),
         vue(),
     ],
     css: {
